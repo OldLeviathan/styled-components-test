a3505e74aa940bfd5dcff0a2d1d7fdfa4c694029
OldLeviathan <djkm.20152@gmail.com>
2022-07-28 13:20:05 +0500

    Full commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import styled from "styled-components";
+import Title from "./components/Title";
+import Flex from "./components/Flex";
+import Console from "./components/Console";
+import Button from "./components/Button";
+
+const AppWrapper = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  padding: 2rem;
+  background: black;
+`
+
+const App = () => {
+    return (
+        <AppWrapper>
+            <Flex justify='center'>
+                <Title> Console cmd 2022. LEVIATHAN </Title>
+            </Flex>
+            <Flex direction="column" margin="20px 0">
+                <Console/>
+                <Button outlined color='red' align='flex-end'>Отправить</Button>
+            </Flex>
+        </AppWrapper>
+    );
+};
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import styled, {createGlobalStyle, ThemeProvider} from "styled-components";
+
+const Global = createGlobalStyle`
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: Consolas;
+}
+`
+
+const theme = {
+    colors:{
+        primary: 'green',
+        secondary: 'red'
+    }
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <ThemeProvider theme={theme}>
+      <Global/>
+      <App/>
+  </ThemeProvider>
+);
